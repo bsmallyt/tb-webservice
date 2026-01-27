1ae16478f779b0f2d53e3bb0dbc1d2cf2bad7be8
bsmallyt <benjaminsmallyt@gmail.com>
2026-01-27 00:48:48 -0600

    test login

--- trulyben/src/app/components/home/home.page.ts
+++ trulyben/src/app/components/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,11 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  private keycloak = inject(Keycloak);
+
   constructor() {}
+
+  login() {
+    this.keycloak.login();
+  }
 }
